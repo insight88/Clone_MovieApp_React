9ab0c9b0139107090fd9abfb6ad0b41eedd3d3e5
insight88 <kpkim1988@gmail.com>
2020-09-03 14:21:36 +0900

    Updates

---
+++ precache-manifest.87b15d8fc34b092398e70cd03640df84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2713ea8ca5864be57af8851183c7c44b",
+    "url": "/clone_movieapp_react/index.html"
+  },
+  {
+    "revision": "554f4c5572262c3b6500",
+    "url": "/clone_movieapp_react/static/css/main.7729a6ea.chunk.css"
+  },
+  {
+    "revision": "d93fe400992f7dc65d30",
+    "url": "/clone_movieapp_react/static/js/2.2cb873af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clone_movieapp_react/static/js/2.2cb873af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "554f4c5572262c3b6500",
+    "url": "/clone_movieapp_react/static/js/main.cc3a3cc3.chunk.js"
+  },
+  {
+    "revision": "bc5d7a71ba756256c8ab",
+    "url": "/clone_movieapp_react/static/js/runtime-main.34f9dc2f.js"
+  }
+]);
