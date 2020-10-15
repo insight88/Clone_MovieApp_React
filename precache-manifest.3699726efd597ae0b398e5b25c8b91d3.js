3ac80930d477b5498db1d3c11d6074bee244e1d4
insight88 <64120339+insight88@users.noreply.github.com>
2020-10-15 14:11:47 +0900

    Updates

---
+++ precache-manifest.3699726efd597ae0b398e5b25c8b91d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd95e9aa8d37283117439f51a055e82c",
+    "url": "/clone-movie.app-react/index.html"
+  },
+  {
+    "revision": "c0c129ee653c4329c7a1",
+    "url": "/clone-movie.app-react/static/css/main.a583c5fa.chunk.css"
+  },
+  {
+    "revision": "da305a7e74d9856d308b",
+    "url": "/clone-movie.app-react/static/js/2.d8c9a3f6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/clone-movie.app-react/static/js/2.d8c9a3f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0c129ee653c4329c7a1",
+    "url": "/clone-movie.app-react/static/js/main.b224370a.chunk.js"
+  },
+  {
+    "revision": "e878b174c54fe35e0d03",
+    "url": "/clone-movie.app-react/static/js/runtime-main.23f6dc5b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clone_movieapp_react/precache-manifest.d574bcbee9f785075c070648a1976ff9.js"
+  "/clone-movie.app-react/precache-manifest.3699726efd597ae0b398e5b25c8b91d3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clone_movieapp_react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clone-movie.app-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
